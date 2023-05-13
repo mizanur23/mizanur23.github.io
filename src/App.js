2c5bdef8a1a30750b538cbee9af172379dded868
Mizanur Rahman Rony <rmizanurrahman76@gmail.com>
2023-05-14 01:02:20 +0600

    creating coming soon page v1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    
+      <CircularProgress />
+    
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Something awesome is coming soon
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
